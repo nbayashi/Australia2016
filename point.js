0f9a7bf82fd8842f6d2b6259fd80ce43372b2e02
bayashi <naoya_nstyle@hotmail.co.jp>
2016-05-12 19:45:15 +0900

    Add files via upload

---
+++ point.js
@@ -0,0 +1,447 @@
+var viewer = new Cesium.Viewer('cesiumContainer');
+var czml = [
+  {
+    "id" : "document",
+    "name" : "fukuchiyama",
+    "version" : "1.0"
+  }, {
+    "id" : "1",
+    "name" : "KÇ≥ÇÒ",
+    "description" : "í≤ç∏ÉGÉäÉAäwçZé¸ï”í≤ç∏ínãÊñxínãÊéÅñºKÇ≥ÇÒê´ï èóê´ãèèZîNêîê∂Ç‹ÇÍÇΩéûÇ©ÇÁÇVÇO?ÇWÇOîNçãâJéûÇÃàÛè€ê¨î¸çÇçZÇÃÇ†ÇΩÇËÇ©ÇÁâJÇ™Ç∑Ç≤Ç©Ç¡ÇΩÅBåoå±ÇµÇΩÇ±Ç∆Ç™Ç»Ç©Ç¡ÇΩÇÃÇ≈ëÂïœÇæÇ¡ÇΩÅBÇ«ÇÃÇÊÇ§Ç…âﬂÇ≤Ç≥ÇÍÇƒÇ¢ÇΩÇ©â∆Ç…Ç¢ÇÈÇµÇ©èoóàÇ»Ç©Ç¡ÇΩÅBêZêÖîÌäQèÛãµàÍã{ê_é–ÇÃêŒÇÃÇ∆Ç±ÇÎÇ‹Ç≈ÉMÉäÉMÉäëÂïœÇæÇ¡ÇΩÇ±Ç∆çÇå©ÇÃå©ï®",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1320894411087,35.28799163469748,30
+      ]
+    }
+  },{
+    "id" : "2",
+    "name" : "MÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.130594,35.288045,30
+      ]
+    }
+  },{
+    "id" : "3",
+    "name" : "HÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1297291543509,35.29059108578114,30
+      ]
+    }
+  },{
+    "id" : "4",
+    "name" : "FÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1291249538861,35.29101553129239,30
+      ]
+    }
+  },{
+    "id" : "5",
+    "name" : "SÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1306683058458,35.28645127024259,30
+      ]
+    }
+  },{
+    "id" : "6",
+    "name" : "MÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1181093768303,35.29390321162933,30
+      ]
+    }
+  },{
+    "id" : "7",
+    "name" : "AÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/a.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1194179846911,35.29378470577179,30
+      ]
+    }
+  },{
+    "id" : "8",
+    "name" : "TÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/t.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1171880924785,35.29568045589838,30
+      ]
+    }
+  },{
+    "id" : "9",
+    "name" : "âwàıÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.118431462511,35.29605795858646,30
+      ]
+    }
+  },{
+    "id" : "10",
+    "name" : "âwàıÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.118964,35.296067,30
+      ]
+    }
+  },{
+    "id" : "11",
+    "name" : "KÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.129778,35.298046,30
+      ]
+    }
+  },{
+    "id" : "12",
+    "name" : "AÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.131259,35.297258,30
+      ]
+    }
+  },{
+    "id" : "12",
+    "name" : "YÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/y.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1119291148579,35.31039213781048,30
+      ]
+    }
+  },{
+    "id" : "13",
+    "name" : "NÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/n.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.1121741359157,35.31257401785641,30
+      ]
+    }
+  },{
+    "id" : "14",
+    "name" : "FÇ≥ÇÒ",
+    "description" : "test",
+    "billboard" : {
+      "image" : "http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/img/noimage.png",
+      "scale" : 0.2
+    },
+    "label" : {
+      "fillColor" : {
+        "rgba" : [0, 0, 0, 255]
+      },
+      "font" : "12pt Lucida Console",
+      "horizontalOrigin" : "CENTER",
+      "outlineColor" : {
+        "rgba":[0, 0, 0, 255]
+      },
+      "outlineWidth" : 1.2,
+      "pixelOffset" : {
+        "cartesian2" : [0, 55]
+      },
+      "style" : "FILL_AND_OUTLINE",
+      "text" : ""
+    },
+    "position" : {
+      "cartographicDegrees": [
+        135.111561,35.310111,30
+      ]
+    }
+  }
+];
+viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/tokyo-demo/cesium-starter-app-master/Source/water_area.kml'));
+viewer.camera.flyTo({   
+     destination : Cesium.Cartesian3.fromDegrees(135.1320894411087,35.28799163469748,8000)});
